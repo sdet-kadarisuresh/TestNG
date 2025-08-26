d9246bc428de9b6735010c987fab060d0ff33f82
sdet-kadarisuresh <kadarisureshsdet@gmail.com>
2025-08-26 16:25:57 +0530

    first commit

--- src/test/java/annotations/FirstTest.java
+++ src/test/java/annotations/FirstTest.java
@@ -1,5 +1,29 @@
 package annotations;
 
-public class FirstTest {
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+public class FirstTest {
+	
+	@Test
+	public void testHello()
+	{
+		System.out.println("hello test");
+		
+		
+	}
+	
+	@Test
+	public void testOne() { System.out.println("Test 1"); }
+
+	@Test
+	public void testTwo() { System.out.println("Test 2"); }
+
+	@Test
+	public void testThree() { System.out.println("Test 3"); }
+	
+	
+	
+	
 }
---
+++ src/test/java/annotations/SecondTest.java
@@ -0,0 +1,40 @@
+package annotations;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@BeforeClass
+	public void setupClass() {
+		System.out.println("BeforeClass: Open Browser");
+	}
+
+	@BeforeMethod
+	public void setupMethod() {
+		System.out.println("BeforeMethod: Navigate to URL");
+	}
+
+	@Test
+	public void loginTest() {
+		System.out.println("Executing Login Test");
+	}
+
+	@Test
+	public void searchTest() {
+		System.out.println("Executing Search Test");
+	}
+
+	@AfterMethod
+	public void teardownMethod() {
+		System.out.println("AfterMethod: Logout");
+	}
+
+	@AfterClass
+	public void teardownClass() {
+		System.out.println("AfterClass: Close Browser");
+	}
+}
